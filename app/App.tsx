255883e03d4c1d238bba3c6694d7b7bdc16f3d96
Yuriy Dzhavala <ydzhavala@adnoc.ae>
2024-05-01 20:20:31 +0300

    add the Error boundary component

--- app/App.tsx
+++ app/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { PhotoEditorProvider } from "./context/PhotoEditorContext";
 import { ImageOffsetManagementProvider } from "./context/ImageOffsetManagementContext";
 import { PhotoEditor } from "./components/PhotoEditor";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 export default function App() {
   return (
     <PhotoEditorProvider>
       <ImageOffsetManagementProvider>
-        <PhotoEditor />
+        <ErrorBoundary fallback={<div>Error occurred!</div>}>
+          <PhotoEditor />
+        </ErrorBoundary>
       </ImageOffsetManagementProvider>
     </PhotoEditorProvider>
   );
